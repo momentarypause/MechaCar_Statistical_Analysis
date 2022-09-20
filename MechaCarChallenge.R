383eaa8cc8ca2cafd38d8713303cd6f63e9c163f
Mandy <momentarypause@gmail.com>
2022-09-20 14:07:51 -0400

    additional challenge code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -22,3 +22,23 @@ total_summary <- suspension %>% summarize (Mean=mean(PSI), Median=median(PSI), V
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize (Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
 
+# DELIVERABLE 3
+
+#t.tests 
+# H0: The true mean of PSI = 1500
+# Ha: The true mean of PSI =/= 1500
+
+# One Sample T-Test against potential population
+t.test((suspension$PSI), mu=1500)
+
+# Subsets of sample against the population
+# Lot 1
+t.test(subset(suspension, Manufacturing_Lot == 'Lot1', PSI), mu=1500)
+
+# Lot 2
+t.test(subset(suspension, Manufacturing_Lot == 'Lot2', PSI), mu=1500)
+
+# Lot 3
+t.test(subset(suspension, Manufacturing_Lot == 'Lot3', PSI), mu=1500)
+
+       
